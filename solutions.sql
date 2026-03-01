4c695a2e973bad92e1ac1748c21294c16540da13
MattiasLarsenn <mattiaslarsen@hotmail.com>
2026-03-01 18:34:10 +0100

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,3 +1,5 @@
+-- Single Table Assignments
+
 -- 1. Find the employee number for employees named MARTIN.
 SELECT employee_number
 FROM employees
@@ -30,13 +32,75 @@ FROM employees
 WHERE hire_date < '1981-05-01';
 
 -- 7. List employees sorted by salary in descending order (i.e. highest salary first).
+SELECT *
+FROM employees
+ORDER BY salary DESC;
 
 -- 8. List departments sorted by location.
+SELECT *
+FROM departments
+ORDER BY office_location;
 
 -- 9. Find name of the department located in New York.
+SELECT department_name
+FROM departments
+WHERE office_location = 'NEW YORK';
 
 -- 10. You have proven your worth at the company. Your colleague comes to you trying to remember what's-his-name. It starts with a J and ends with S. Can you help her?
+SELECT employee_name
+FROM employees
+WHERE employee_name LIKE 'J%S';
 
 -- 11. Maybe that wasn't helpful. "Oh yeah, I remember now!" they say and tell you that he is a manager.
+SELECT employee_name
+FROM employees
+WHERE employee_name LIKE 'J%S' AND job_title = 'MANAGER';
 
 -- 12. How many employees are there in each department?
+
+SELECT department_number, COUNT(*)
+FROM employees
+GROUP BY department_number;
+
+-- Aggregate functions
+
+-- 1. List the number of employees.
+
+SELECT COUNT(*) AS Total_employees
+FROM employees;
+
+-- 2. List the sum of all salaries (excluding commission which are bonuses added to the base salaries).
+
+SELECT SUM(salary) AS Total_Salary
+FROM employees;
+
+-- 3. List the average salary for employees in department 20.
+
+SELECT AVG(salary) AS Average_Salary
+FROM employees
+WHERE department_number = 20;
+
+-- 4. List the unique job titles in the company.
+
+SELECT DISTINCT job_title
+FROM employees;
+
+-- 5. List the number of employees in each department.
+
+SELECT department_number, COUNT(*) AS Number_of_employees
+FROM employees
+GROUP BY department_number;
+
+-- 6. List in decreasing order the maximum salary in each department together with the department number.
+
+SELECT department_number, MAX(salary) AS best_paid
+FROM employees
+GROUP BY department_number
+ORDER BY best_paid DESC;
+
+-- 7. List total sum of salary and commission for all employees.
+
+SELECT SUM(salary) + SUM(ifnull(commission, 0)) AS total_income
+FROM employees;
+
+
